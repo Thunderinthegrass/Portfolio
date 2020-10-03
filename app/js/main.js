ec557082011aace2ff35dae259b8f38f0be5a6de
ThunderInTheGrass <propeller8924@gmail.com>
2020-10-03 17:05:32 +0800

    finished language switcher

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,10 @@ let home = document.querySelector('.home');
 let aboutMe = document.querySelector('.about-me');
 let myPortfolio = document.querySelector('.my-portfolio');
 let myContacts = document.querySelector('.my-contacts');
+let paintSmearMask = document.querySelector('.paint-smear-mask');
+let paintSmearRu = document.querySelector('.paint-smear-ru');
+let paintSmearEn = document.querySelector('.paint-smear-en');
+let paintSmearMaskEn = document.querySelector('.paint-smear-mask-en');
 
 changeLanguage.onclick = () => {
   if (changeLanguage.checked) {
@@ -11,11 +15,24 @@ changeLanguage.onclick = () => {
     aboutMe.innerHTML = ('About me');
     myPortfolio.innerHTML = ('Portfolio');
     myContacts.innerHTML = ('Contacts');
+
+    paintSmearMask.classList.remove('go-right');
+    paintSmearRu.classList.add('disp-none');
+    
+
+    paintSmearEn.classList.remove('disp-none');
+    paintSmearMaskEn.classList.add('go-right');
   }
   else {
     home.innerHTML = ('Главная');
     aboutMe.innerHTML = ('Обо мне');
     myPortfolio.innerHTML = ('Портфолио');
     myContacts.innerHTML = ('Контакты');
+
+    paintSmearRu.classList.remove('disp-none');
+    paintSmearMask.classList.add('go-right');
+
+    paintSmearMaskEn.classList.remove('go-right');
+    paintSmearEn.classList.add('disp-none');
   }
 }
