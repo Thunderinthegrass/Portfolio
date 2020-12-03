f8660712eb3ac0ffaef46578dc3ebeec2026f455
ThunderInTheGrass <propeller8924@gmail.com>
2020-12-03 23:44:03 +0800

    make logo

--- app/js/main.js
+++ app/js/main.js
@@ -103,7 +103,7 @@ if (animItems.length > 0) {
       const animItem = animItems[k];
       const animItemHeight = animItem.offsetHeight; 
       const animItemOffset = offset(animItem).top; 
-      const animStart = 10;
+      const animStart = 2;
 
       let animItemPoint = window.innerHeight - animItemHeight / animStart;
       if (animItemHeight > window.innerHeight) {
