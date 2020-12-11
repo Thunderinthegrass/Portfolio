e59e47af271beb032c96a4924204318ebe54e97e
ThunderInTheGrass <propeller8924@gmail.com>
2020-12-11 23:23:00 +0800

    made hamberger-menu, continue responsive header

--- app/js/main.js
+++ app/js/main.js
@@ -1,52 +1,65 @@
 // change language
 let changeLanguage = document.querySelector('.change__language');
-let home = document.querySelector('.home');
-let aboutMe = document.querySelector('.about-me');
-let myPortfolio = document.querySelector('.my-portfolio');
-let myContacts = document.querySelector('.my-contacts');
-let paintSmearMask = document.querySelector('.paint-smear-mask');
-let paintSmearRu = document.querySelector('.paint-smear-ru');
-let paintSmearEn = document.querySelector('.paint-smear-en');
-let paintSmearMaskEn = document.querySelector('.paint-smear-mask-en');
-let headerTitle = document.querySelector('.header__title');
+let contentLangRu = [
+  'наверх',
+  'главная',
+  'обо мне',
+  'портфолио',
+  'выключить глаз',
+  'роман насачевский',
+  'верстальщик',
+  'роман насачевский',
+  'верстальщик'
+];
+
+let contentLangEn = [
+  'up',
+  'home',
+  'about me',
+  'portfolio',
+  'turn off the eye',
+  'roman nasachevsky',
+  'layout designer',
+  'roman nasachevsky',
+  'layout designer'
+];
+
+let contentItem = document.querySelectorAll('._lang');
 
 changeLanguage.onclick = () => {
-  if (changeLanguage.checked) {
-    home.innerHTML = ('Home');
-    aboutMe.innerHTML = ('About me');
-    myPortfolio.innerHTML = ('Portfolio');
-    myContacts.innerHTML = ('Contacts');
-
-    headerTitle.innerHTML = ('Roman Nasachevsky');
-  }
-  else {
-    home.innerHTML = ('Главная');
-    aboutMe.innerHTML = ('Обо мне');
-    myPortfolio.innerHTML = ('Портфолио');
-    myContacts.innerHTML = ('Контакты');
-
-    headerTitle.innerHTML = ('Роман Насачевский');
+  for (let k = 0; k < contentItem.length; k++) {
+    if (changeLanguage.checked) {
+      contentItem[k].textContent = contentLangEn[k];
+    }
+    else {
+      contentItem[k].textContent = contentLangRu[k];
+    }
   }
 }
 //eye
 let eyeSwitch = document.querySelector('.eye-switch');
 
-eyeSwitch.onclick = function() {
+eyeSwitch.onclick = function () {
   let eye = document.querySelector('.eye');
   if (eyeSwitch.checked) {
     eye.classList.remove('eye-red');
   }
-  else{
+  else {
     eye.classList.add('eye-red');
   }
 }
 
 //hamburger
 let menuBtn = document.querySelector('.menu-btn');
+let menuBtnElem = document.querySelector('.menu-btn__elem');
 let headerMenu = document.querySelector('.header__menu');
 
 menuBtn.onclick = () => {
+  menuBtn.classList.toggle('menu-btn--active');
+  menuBtnElem.classList.toggle('menu-btn__elem--active');
+
   headerMenu.classList.toggle('menu--active');
+
 }
 
 // плавный скролл до якоря
@@ -93,7 +106,7 @@ function btnToTop() {
 
   button.on('click', (e) => {
     e.preventDefault();
-    $('html').animate({scrollTop: 0}, 500);
+    $('html').animate({ scrollTop: 0 }, 500);
   })
 }
 
@@ -107,10 +120,10 @@ const animItems = document.querySelectorAll('._anim-items');
 if (animItems.length > 0) {
   window.addEventListener('scroll', animOnScroll);
   function animOnScroll() {
-    for(let k = 0; k < animItems.length; k++) {
+    for (let k = 0; k < animItems.length; k++) {
       const animItem = animItems[k];
-      const animItemHeight = animItem.offsetHeight; 
-      const animItemOffset = offset(animItem).top; 
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
       const animStart = 2;
 
       let animItemPoint = window.innerHeight - animItemHeight / animStart;
@@ -121,7 +134,7 @@ if (animItems.length > 0) {
       if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
         animItem.classList.add('_active');
       }
-      else{
+      else {
         animItem.classList.remove('_active');
       }
     }
