2318fdd20274e80c6d289df9c511e4def9bfea92
ThunderInTheGrass <propeller8924@gmail.com>
2021-02-17 21:46:25 +0800

    form-wdm

--- dist/js/main.js
+++ dist/js/main.js
@@ -153,110 +153,25 @@ if (animItems.length > 0) {
 }
 
 //form-------------------------------------------------------------
-// $(document).ready(function() {
+$(document).ready(function() {
 
-// 	//E-mail Ajax Send
-// 	$("form").submit(function() { //Change
-// 		var th = $(this);
-// 		$.ajax({
-// 			type: "POST",
-// 			url: "mail.php", //Change
-// 			data: th.serialize()
-// 		}).done(function() {
-// 			alert("Thank you!");
-// 			setTimeout(function() {
-// 				// Done Functions
-// 				th.trigger("reset");
-// 			}, 1000);
-// 		});
-// 		return false;
-// 	});
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 
-// });
+});
 // -----------------------------------------
 
-document.addEventListener('DOMContentLoaded', function() {
-  const form = document.getElementById('form');
-  form.addEventListener('submit', formSend);
-
-  async function formSend(e) {
-    e.preventDefault();
-
-    let error = formValidate(form);
-
-    let formData = new FormData(form);
-
-    if (error === 0) {
-      form.classList.add('_sending');
-      // ================================================
-      $(document).ready(function() {
-
-        //E-mail Ajax Send
-        $("form").submit(function() { //Change
-          var th = $(this);
-          $.ajax({
-            type: "POST",
-            url: "mail.php", //Change
-            data: th.serialize()
-          }).done(function() {
-            alert("Thank you!");
-            setTimeout(function() {
-              // Done Functions
-              th.trigger("reset");
-            }, 1000);
-          });
-          return false;
-        });
-      
-      });
-      // =============================================
-
-      }
-    else {
-      alert('Будьте любезны, заполните обязательные поля');
-    }
-  }
-
-  function formValidate(form) {
-    let error = 0;
-    let formReq = document.querySelectorAll('._req');
-
-    for (let i = 0; i < formReq.length; i++) {
-      const input = formReq[i];
-      formRemoveError(input)
-
-      if (input.classList.contains('_email')) {
-        if (emailTest(input)) {
-          formAddError(input);
-          error++;
-        }
-      }
-      else if (input.getAttribute("type") === "checkbox" && input.checked === false) {
-        formAddError(input);
-        error++;
-      }
-      else {
-        if (input.value === '') {
-          formAddError(input);
-            error++;
-        }
-      }
-    }
-    return error;
-  }
-
-  function formAddError(input) {
-    input.parentElement.classList.add('_error');
-    input.classList.add('_error');
-  }
-  function formRemoveError(input) {
-    input.parentElement.classList.remove('_error');
-    input.classList.remove('_error');
-  }
-
-  function emailTest(input) {
-    return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-  }
-
-  
-})
